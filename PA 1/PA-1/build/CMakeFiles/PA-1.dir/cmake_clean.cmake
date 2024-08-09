78f9e253b72d0597a0d237d58ac44ebe8f0c68ba
Mark 篠崎 <44478527+MarkShinozaki@users.noreply.github.com>
2024-08-09 12:45:37 -0400

    Add files via upload

---
+++ PA 1/PA-1/build/CMakeFiles/PA-1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA-1.dir/main.cpp.o"
+  "PA-1"
+  "PA-1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA-1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
