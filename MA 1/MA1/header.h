64c0022a6b917404abe980ba2085841a9cd5bc50
Mark 篠崎 <44478527+MarkShinozaki@users.noreply.github.com>
2024-08-09 12:42:53 -0400

    Add files via upload

---
+++ MA 1/MA1/header.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+// define default capacity of the queue
+#define SIZE 10
+
---
+++ MA 1/MA1/testQueue.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+#include "queue.h"
+
+
+void testDequeueEmpty(void);
+void testDequeueFull(void);
+void testEnqueueEmpty(void);
+void testEnqueueFull(void);
+void testPeekEmpty(void);
+void testPeekFull(void);
+
+void testSize(void);
+void testIsEmpty(void);
+void testIsFull(void);
